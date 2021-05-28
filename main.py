e5d2f45e2acb117e58caecc5e50fa6586bfd7f4d
Paul Montecot Grall <paul.montecotgrall@cri-paris.org>
2021-05-28 12:39:48 +0200

    Plotly Linechart showing cases by country

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ import datetime
 import time
 
 df_case = pd.read_csv(r'C:\Users\CRI User\Documents\GitHub\Covid_Dash_OpenSource\Datas\Clean_Case.csv',header=1)
+df_case = df_case.T
+df_case.columns = df_case.iloc[1]
+df_case= df_case.reset_index()
+df_case=df_case.drop([0,1])
+df_case=df_case.rename(columns={"index": "Date"})
 st.title("OUR FIRST DASHBOARD HEHE")
 
 st.checkbox('first checkbox')
 print(df_case)
 
 
-case = st.selectbox('choose country',df_case['Date'])
+case = st.selectbox('choose country',df_case.columns)
 
-df = px.data.gapminder().query("continent=='Oceania'")
-fig = px.line(df_case, x=df.columns, y=case)
+print(case)
+
+fig = px.line(df_case, x=df_case['Date'], y=case)
 st.write(fig)
