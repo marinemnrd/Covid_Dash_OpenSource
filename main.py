37c51fc7ca7f964b8f4bff163f06fdc9bbb96fd7
Paul Montecot Grall <paul.montecotgrall@cri-paris.org>
2021-05-28 11:44:22 +0200

    Country Selection selectbox

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import pandas as pd
 import streamlit as st
-import plotly as px
+import pandas as pd
+# from plotly import plotly
+import plotly.express as px
 import numpy as np
 import datetime
 import time
 
+df_case = pd.read_csv(r'C:\Users\CRI User\Documents\GitHub\Covid_Dash_OpenSource\Datas\Clean_Case.csv',header=1)
 st.title("OUR FIRST DASHBOARD HEHE")
 
-
 st.checkbox('first checkbox')
+print(df_case)
+
+
+case = st.selectbox('choose country',df_case['Date'])
+
+df = px.data.gapminder().query("continent=='Oceania'")
+fig = px.line(df_case, x=df.columns, y=case)
+st.write(fig)
