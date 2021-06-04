08951dce87dbaf4964eac668183cd72da5e86e79
Elizabeth Afolabi <elizabeth.afolabi@joza-it.fr>
2021-06-04 10:38:02 +0200

    Update sidebar

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 import pandas as pd
 # from plotly import plotly
@@ -6,6 +5,34 @@ import plotly.express as px
 import numpy as np
 import datetime
 import time
+
+st.sidebar.title("Menu")
+st.sidebar.radio("Navigate", ["Home", "Data", "Dashboard", "About"])
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    Thus app is maintained by Team 6, Paul, Marine and Elizabeth.
+    """
+)
+st.sidebar.title("Comment")
+st.sidebar.info("Feel free to comment on our work. The github link can be found "
+                "[here](https://github.com/marinemnrd/Covid_Dash_OpenSource)")
+
+
+st.title("OUR FIRST DASHBOARD HEHE")
+st.write("""
+This web application will serve to analyze and visualize the spread of COVID-19""")
+st.markdown("## Symptoms")
+st.markdown(("* Fever or chills\n* Cough\n"
+             "* Shortness of breath or difficulty breathing\n"
+             "* Fatigue\n"
+             "* Muscle or body aches\n"
+             "* Headache\n"
+             "* Loss of taste or smell\n"
+             "* Sore throat\n"
+             "* Congestion or runny nose\n"
+             "* Nausea or vomiting\n"
+             "* Diarrhea"))
 
 df_case = pd.read_csv(r'C:\Users\CRI User\Documents\GitHub\Covid_Dash_OpenSource\Datas\Clean_Case.csv',header=1)
 df_case = df_case.T
